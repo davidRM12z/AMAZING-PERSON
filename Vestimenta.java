72c2f07bfa84961ed58f17ddf6b3c393b2100e80
David <davidrojomar12@gamil.com>
2023-01-19 11:03:49 +0100

    Commit 1

---
+++ Vestimenta.java
@@ -0,0 +1,16 @@
+public class Vestimenta
+{
+    // Color ropa
+    private String color;
+    // Marca ropa
+    private String marca;
+
+    /**
+     * Constructor for objects of class Vestimenta
+     */
+    public Vestimenta(String colorRopa, String marcaRopa)
+    {
+        color = colorRopa;
+        marca = marcaRopa;
+    }
+}
