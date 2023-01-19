259d06d8b54d4613914640368fcea8b3b96b8d34
DeianMarioRus <deianrus04@gmail.com>
2023-01-19 11:18:53 +0100

    Commit 4

---
+++ Persona.java
@@ -0,0 +1,14 @@
+public class Persona
+{
+    // instance variables - replace the example below with your own
+    private int edad;
+    private int altura;
+    private Vestimenta ropa;
+
+    
+    public Persona(){
+        
+    }
+
+    
+}
